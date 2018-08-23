cbfc71173e4615dcb0f8adb82c335cf28c215a7c
kcrash201 <1008917@student.sae.edu.au>
2018-08-23 20:49:22 +0800

    Fungas and little bit of camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	var cam : GameObject;
+
+	void Start() {
+
+		cam = GameObject.Find("Main Camera");
+
+	}
+
+	void Update() {
+		var movement = Vector3.zero;
+
+		if ((Input.GetKey("a")) && (cam.transform.x > -10) && (cam.transform.x < 10))
+		{
+			movement.x--;
+		}
+
+		if ((Input.GetKey("d")) && (cam.transform.x > -10) && (cam.transform.x < 10))
+		{
+			movement.x++;
+		}
+
+		transform.Translate(movement * speed * Time.deltaTime, Space.Self);
+
+	}
+}
+
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -59,6 +59,7 @@ public class PlayerControl : MonoBehaviour {
 	}
 
 
+
 		//--------------------------------------
 		// Change the players animation state
 		//--------------------------------------
